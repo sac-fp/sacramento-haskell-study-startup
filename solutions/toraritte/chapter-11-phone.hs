0a71e23f2c9f1a70ee5b9d4c66e1899683677dca
Attila Gulyas <toraritte@gmail.com>
2018-03-25 23:21:28 -0700

    Finish(?) phone exercise

--- solutions/toraritte/chapter-11-phone.hs
+++ solutions/toraritte/chapter-11-phone.hs
@@ -12,6 +12,8 @@ data DaPhone = DaPhone [Button] deriving (Show, Eq)
 -- Valid presses: 1 and up
 type Presses = Int
 
+-- 1 --- Create data structure
+------------------------------
 phone :: DaPhone
 phone = DaPhone [ ('1', "1"    ),  ('2', "abc2"), ('3', "def3")
                 , ('4', "ghi4" ),  ('5', "jkl5"), ('6', "mno6")
@@ -19,6 +21,8 @@ phone = DaPhone [ ('1', "1"    ),  ('2', "abc2"), ('3', "def3")
                 , ('*', "*^"   ),  ('0', " +_0" ), ('#', "#.,")
                 ]
 
+-- 2 --- Convert conversation into keypresses
+---------------------------------------------
 presses :: Button -> Char -> [(Digit, Presses)]
 presses (digit, xs) c = ifUpper ++ [(digit, (getPresses xs (toLower c) 1))]
   where
@@ -45,3 +49,72 @@ reverseTaps phone c = presses (button phone) c
 cellPhonesDead :: DaPhone -> String -> [(Digit, Presses)]
 cellPhonesDead phone str =
   foldr (\char list -> (reverseTaps phone char) ++ list) [] str
+
+--- Test conversion
+convo :: [String]
+convo =
+  ["Wanna play 20 questions",
+  "Ya",
+  "U 1st haha",
+  "Lol ok. Have u ever tasted alcohol",
+  "Lol ya",
+  "Wow ur cool haha. Ur turn",
+  "Ok. Do u think I am pretty Lol",
+  "Lol ya",
+  "Just making sure rofl ur turn"]
+
+testConvo :: [String] -> [[(Digit, Presses)]]
+testConvo = map (cellPhonesDead phone)
+
+-- 3 ---
+--------
+fingerTaps :: [(Digit, Presses)] -> Presses
+fingerTaps = foldr sumPresses 0
+  where
+    sumPresses (_, p) b = b + p
+
+testFingerTaps :: [String] -> [Presses]
+testFingerTaps = map countTaps
+  where
+    countTaps = fingerTaps . (cellPhonesDead phone)
+
+-- 4 ---
+--------
+-- Maybe I didn't understand this correctly, but there is no need to use
+-- any above functions for `mostPopularLetter` - the one below would suffice.
+mostPopularLetter :: String -> Char
+mostPopularLetter str = snd . maximum $ foldr getCharStats [] lowStr
+  where
+    lowStr = map toLower str
+    getCharStats char b =
+      case (filter (\(_,c) -> char == c) b) of
+        [] ->
+          b ++ [((length . (filter (==char)) $ lowStr), char)]
+        xs  ->
+          b ++ []
+
+-- On the other hand, characters could tie for the first place, but an
+-- unambiguous cost can be assigned to each character with the dialpad
+-- representation (e.g., number of taps).
+--
+-- By generalizing the above function:
+type Cost = Int
+
+-- charFrequency :: String -> (Cost, Char)
+charFrequency costFunction str = foldr getCharStats [] str
+  where
+    getCharStats char b =
+      case (filter (\(_,c) -> char == c) b) of
+        [] ->
+          b ++ [((costFunction char str), char)]
+        xs  ->
+          b ++ []
+
+-- Reimplementing `mostPopularLetter` with it:
+mostPopularLetter2 :: String -> Char
+mostPopularLetter2 str = snd . maximum $ charFrequency costF (lower str)
+  where
+    lower = map toLower
+    costF char str = length . (filter (== char)) $ (lower str)
+
+-- (...)
